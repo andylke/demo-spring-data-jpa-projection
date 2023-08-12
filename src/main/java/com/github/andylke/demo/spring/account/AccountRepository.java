3b26c33ecfa1b202a41c8c5ae5c5448d909a9dea
Andy Lian <andylke@gmail.com>
2023-08-12 21:40:16 +0800

    Remove account.

--- src/main/java/com/github/andylke/demo/spring/account/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.andylke.demo.spring.account;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-  public Account getByUserCode(String userCode);
-}
